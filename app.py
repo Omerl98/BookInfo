2f108ad073cf7da6a215be1f3d74906cf1230620
Omer Lehrer <Omer.Lehrer98@gmail.com>
2021-11-18 17:44:53 +0200

    second commit

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+books = [
+    {
+        'name':"Harry Potter and the Philosopher's Stone",
+        'author':'JK Rowling',
+        'publisher':'Bloomsbury',
+        'publishDate':'1997',
+        'cover':"/static/pics/philosophers_Stone.png"
+    },
+    {
+        'name':'Harry Potter and the Chamber of Secrets',
+        'author':'JK Rowling',
+        'publisher':'Bloomsbury',
+        'publishDate':'1998',
+        'cover':"/static/pics/chamber_of_secrets.png"
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', books = books, title = "Home")
 
 if __name__ == "__main__":
     app.run(debug=True)
